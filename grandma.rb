ed8bf1cb881ba573255dd14f0c5c581cb18b1617
Spencer Cambern <cambern134@gmail.com>
2020-05-01 06:23:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,4 @@
-if speak_to_grandma == ('I LOVE YOU GRANDMA!')
-  puts "I LOVE YOU TOO PUMPKIN!"
-end
+def speak_to_grandma(Phrase)
+ if phrase == "I LOVE YOU GRANDMA!"
+   puts "I LOVE YOU TOO PUMPKIN!"
+ end
